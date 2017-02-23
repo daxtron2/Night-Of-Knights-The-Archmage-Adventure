e372dad865415f442d64a2fa5c1773e8af47c466
TJ Wolschon <tjw3948@g.rit.edu>
2017-02-22 19:24:23 -0500

    Commented the new code from my last commit
    Signed-off-by: TJ Wolschon <tjw3948@g.rit.edu>

--- GDAPS2Game/Game1.cs
+++ GDAPS2Game/Game1.cs
@@ -22,7 +22,7 @@ namespace GDAPS2Game
         KeyboardState oldKState;
         MouseState mState;
         bool paused;//true = paused, false = in game
-        bool firstMenu;
+        bool firstMenu;//true = start menu false = pause menu
         public Game1()
         {
             graphics = new GraphicsDeviceManager(this);
@@ -30,9 +30,9 @@ namespace GDAPS2Game
             graphics.PreferredBackBufferWidth = 1600;//width of window
             graphics.PreferredBackBufferHeight = 900;//height of window
             menu = new Menu();
-            paused = true;
-            IsMouseVisible = true;
-            firstMenu = true;
+            paused = true;//whether or not the game is in the menu state
+            IsMouseVisible = true;//mouse is visible
+            firstMenu = true;//whether or not it's the start menu or pause menu
 
 
         }
@@ -57,7 +57,7 @@ namespace GDAPS2Game
         {
             // Create a new SpriteBatch, which can be used to draw textures.
             spriteBatch = new SpriteBatch(GraphicsDevice);
-            mainFont = Content.Load<SpriteFont>("mainFont");
+            mainFont = Content.Load<SpriteFont>("mainFont");//font used in the menus
 
         }
 
@@ -81,35 +81,35 @@ namespace GDAPS2Game
         {
             kState = Keyboard.GetState();//first thing
             mState = Mouse.GetState();//second thing
-            if (paused == false)
+            if (paused == false)//if in game
             {
-                if (kState.IsKeyDown(Keys.Escape) && oldKState.IsKeyUp(Keys.Escape))
+                if (kState.IsKeyDown(Keys.Escape) && oldKState.IsKeyUp(Keys.Escape))//escape is pressed
                 {
-                    paused = true;
+                    paused = true;// go to pause menu
                 }
             }
-            else if(paused == true)
+            else if(paused == true)//if in pause menu
             {
-                menu.Input();
-                if (kState.IsKeyDown(Keys.Escape) && oldKState.IsKeyUp(Keys.Escape))
+                menu.Input();//check for menu input
+                if (kState.IsKeyDown(Keys.Escape) && oldKState.IsKeyUp(Keys.Escape))//if escape is pressed
                 {
-                    paused = false;
+                    paused = false;//exit the menu
                 }
             }
-            if (kState.IsKeyDown(Keys.Enter))
+            if (kState.IsKeyDown(Keys.Enter))//if enter is pressed
             {
-                switch (menu.SelectionIndex)
+                switch (menu.SelectionIndex)//check what is currently selected
                 {
-                    case 0:
-                        paused = false;
-                        firstMenu = false;
+                    case 0://if the top button, play/resume game, is selected
+                        paused = false;//unpause game
+                        firstMenu = false;//no longer first menu, if not already
                         break;
-                    case 1:
-                        Exit();
+                    case 1://if exit game is selected
+                        Exit();//close the game
                         break;
-                    default:
-                        paused = true;
-                        break;
+                    default://if something other than the two we currently have is selected
+                        paused = true;//leave it paused
+                        break;//do nothing
                 }
             }
 
@@ -129,27 +129,29 @@ namespace GDAPS2Game
             spriteBatch.Begin();//Draw after this
             if(paused == true)//if in menu, i.e. paused
             {
-                if (firstMenu == true)
+                if (firstMenu == true)//if this is the first menu,
                 {
                     spriteBatch.DrawString(mainFont, "Start Menu", new Vector2(800 - (mainFont.MeasureString("Start Menu").Length() / 2), 50), Color.Black); //centers text at 200 = y
-                    
+                    //Display "Start Menu" instead of Pause menu
                 }
-                else if(firstMenu == false)
+                else if(firstMenu == false)//if not first menu
                 {
                     spriteBatch.DrawString(mainFont, "Pause Menu", new Vector2(800 - (mainFont.MeasureString("Pause Menu").Length() / 2), 50), Color.Black); //centers text at 200 = y
-
+                    //display "Pause Menu" instead of start menu
                 }
-                spriteBatch.DrawString(mainFont, menu.SelectionIndex.ToString(), new Vector2(200, 100), Color.Black);//debug print
-                spriteBatch.DrawString(mainFont, "Play Game", new Vector2(667, 100), Color.Black);
-                spriteBatch.DrawString(mainFont, "Exit Game", new Vector2(667, 150), Color.Black);
+                //spriteBatch.DrawString(mainFont, menu.SelectionIndex.ToString(), new Vector2(200, 100), Color.Black);//debug print needed when no graphical indication of selected object is present
+
+                spriteBatch.DrawString(mainFont, "Play Game", new Vector2(667, 100), Color.Black);//draws the play game "button"
+                spriteBatch.DrawString(mainFont, "Exit Game", new Vector2(667, 150), Color.Black);//draws the exit game "button"
+
                 switch (menu.SelectionIndex)//draws two asterisks before and after currently selected item
                 {
-                    case 0:
+                    case 0://places the asterisks with "Play Game"
                         spriteBatch.DrawString(mainFont, "*", new Vector2(640, 105), Color.Black);
                         spriteBatch.DrawString(mainFont, "*", new Vector2(925, 105), Color.Black);
 
                         break;
-                    case 1:
+                    case 1://places the asterisks with "Exit Game"
                         spriteBatch.DrawString(mainFont, "*", new Vector2(640, 155), Color.Black);
                         spriteBatch.DrawString(mainFont, "*", new Vector2(925, 155), Color.Black);
 
--- GDAPS2Game/Menu.cs
+++ GDAPS2Game/Menu.cs
@@ -12,33 +12,33 @@ namespace GDAPS2Game
     class Menu
     {
         private int selectionIndex;//determines which button is selected. 0=play/resume game, 1=exit
-        public int SelectionIndex { get { return selectionIndex; } }
+        public int SelectionIndex { get { return selectionIndex; } }//allows Update() to see selection index
         KeyboardState kState;
         KeyboardState oldKState;
         public Menu()
         {
-            selectionIndex = 0;
+            selectionIndex = 0;//start off hovering over Play Game button
         }
 
         public void Input()
         {
-            kState = Keyboard.GetState();
-            if(kState.IsKeyDown(Keys.Up) && oldKState.IsKeyUp(Keys.Up))
+            kState = Keyboard.GetState();//get the keyboard state everytime it's called
+            if(kState.IsKeyDown(Keys.Up) && oldKState.IsKeyUp(Keys.Up))//if the user presses up, only run once per press
             {
                 if (selectionIndex != 0)//0 is top of menu
                 {
-                    selectionIndex -= 1;
+                    selectionIndex -= 1;//go up one menu item
                 }
-                else if (selectionIndex < 0)
+                else if (selectionIndex < 0)//don't let it go below 0
                 {
                     selectionIndex = 0;
                     //maybe play a sound indicating you cant go higher in the menu
                 }
             }
 
-            if (kState.IsKeyDown(Keys.Down) && oldKState.IsKeyUp(Keys.Down))
+            if (kState.IsKeyDown(Keys.Down) && oldKState.IsKeyUp(Keys.Down))//if down arrow is pressed, only run once per input
             {
-                selectionIndex += 1;
+                selectionIndex += 1;//go down 1 item menu
             }
             //else if (selectionIndex > maxValue)
             //{
