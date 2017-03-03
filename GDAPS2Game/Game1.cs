1eb5c992e8cb04e67f625501b64424c866268a46
TJ Wolschon <tjw3948@g.rit.edu>
2017-03-03 16:43:58 -0500

    "Camera" now follows player past 200px from left side of screen
    Added collision with left side of screen
    
    Fixed a bug with CharacterBox property being in wrong class
    
    Signed-off-by: TJ Wolschon <tjw3948@g.rit.edu>

--- GDAPS2Game/Game1.cs
+++ GDAPS2Game/Game1.cs
@@ -28,6 +28,8 @@ namespace GDAPS2Game
         Texture2D rangedEnemySprite;
         Player player;
         List<Enemy> enemies;
+        Vector3 cameraPos;
+
         const int FLOOR = 750;
         
         enum GameState { Menu, Pause, Game, GameOver}
@@ -38,6 +40,7 @@ namespace GDAPS2Game
             Content.RootDirectory = "Content";
             graphics.PreferredBackBufferWidth = 1600;//width of window
             graphics.PreferredBackBufferHeight = 900;//height of window
+            IsFixedTimeStep = false;
             menu = new Menu();
             currentState = GameState.Menu;
             IsMouseVisible = true;//mouse is visible
@@ -70,6 +73,7 @@ namespace GDAPS2Game
             floorBG = Content.Load<Texture2D>("Background Layer 1");
             playerSprite = Content.Load<Texture2D>("playerSpriteTesting");
             player = new Player(new Rectangle(50, 50, 65, 130), playerSprite);
+
 
         }
 
@@ -129,6 +133,7 @@ namespace GDAPS2Game
             }
 
 
+            cameraPos = new Vector3((player.CharacterBox.X*-1)+200, 0, 0f);
             //last thing
             oldKState = kState;
             base.Update(gameTime);
@@ -141,7 +146,18 @@ namespace GDAPS2Game
         protected override void Draw(GameTime gameTime)
         {
             GraphicsDevice.Clear(Color.CornflowerBlue);
-            spriteBatch.Begin();//Draw after this
+            
+            if (player.CharacterBox.X > 200)//if the player is past 200px on the screen
+            {
+                //the camera will stick with the player along the x coordinate
+                spriteBatch.Begin(SpriteSortMode.Deferred, null, null, null, null, null, Matrix.CreateTranslation(cameraPos));//Draw after this
+            }
+            if (player.CharacterBox.X <= 200)//if they're at or before 200
+            {
+                spriteBatch.Begin();//draw normally with topleft being (0,0)
+            }
+
+
             switch (currentState)
             {
                 case GameState.Game:
@@ -157,7 +173,8 @@ namespace GDAPS2Game
                     DrawMenu(spriteBatch);//Draws the Game Over screen
                     break;
             }
-            spriteBatch.DrawString(mainFont, "X: " + mState.X + " Y: " + mState.Y, new Vector2(25, 25), Color.White);
+            //spriteBatch.DrawString(mainFont, "X: " + mState.X + " Y: " + mState.Y, new Vector2(25, 25), Color.White);//debug, show mouse coords
+            spriteBatch.DrawString(mainFont, "X: " + player.CharacterBox.X + " Y: " + player.CharacterBox.Y, new Vector2(25, 50), Color.White);//debug, show player coords
             spriteBatch.End();//Draw before this
             base.Draw(gameTime);
         }
