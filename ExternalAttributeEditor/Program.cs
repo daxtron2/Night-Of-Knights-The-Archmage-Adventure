77c7205ef0bfe0ab4bbd2683ad52680b37b2e937
TJ Wolschon <tjw3948@g.rit.edu>
2017-03-08 10:02:18 -0500

    Added Binary reader/writer w/ file path that points to a file in game's content
    Signed-off-by: TJ Wolschon <tjw3948@g.rit.edu>

--- ExternalAttributeEditor/Program.cs
+++ ExternalAttributeEditor/Program.cs
@@ -16,7 +16,7 @@ namespace ExternalAttributeEditor
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new ExtAttEdit());
         }
     }
 }
