66f2c9008c2d8c91ddf47731c6f7ae0dd4482f66
TJ Wolschon <tjw3948@g.rit.edu>
2017-03-08 10:39:37 -0500

    Changes Gravity value
    Signed-off-by: TJ Wolschon <tjw3948@g.rit.edu>

--- ExternalAttributeEditor/Form1.Designer.cs
+++ ExternalAttributeEditor/Form1.Designer.cs
@@ -28,27 +28,21 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
             this.Gravity = new System.Windows.Forms.Label();
             this.SaveButton = new System.Windows.Forms.Button();
-            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.GravityUD = new System.Windows.Forms.NumericUpDown();
+            this.LoadButton = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.GravityUD)).BeginInit();
             this.SuspendLayout();
-            // 
-            // numericUpDown1
-            // 
-            this.numericUpDown1.Location = new System.Drawing.Point(21, 32);
-            this.numericUpDown1.Name = "numericUpDown1";
-            this.numericUpDown1.Size = new System.Drawing.Size(120, 20);
-            this.numericUpDown1.TabIndex = 0;
             // 
             // Gravity
             // 
             this.Gravity.AutoSize = true;
             this.Gravity.Location = new System.Drawing.Point(21, 13);
             this.Gravity.Name = "Gravity";
-            this.Gravity.Size = new System.Drawing.Size(35, 13);
+            this.Gravity.Size = new System.Drawing.Size(40, 13);
             this.Gravity.TabIndex = 1;
-            this.Gravity.Text = "label1";
+            this.Gravity.Text = "Gravity";
             // 
             // SaveButton
             // 
@@ -60,28 +54,45 @@
             this.SaveButton.UseVisualStyleBackColor = true;
             this.SaveButton.Click += new System.EventHandler(this.SaveButton_Click);
             // 
+            // GravityUD
+            // 
+            this.GravityUD.Location = new System.Drawing.Point(24, 30);
+            this.GravityUD.Name = "GravityUD";
+            this.GravityUD.Size = new System.Drawing.Size(37, 20);
+            this.GravityUD.TabIndex = 3;
+            // 
+            // LoadButton
+            // 
+            this.LoadButton.Location = new System.Drawing.Point(282, 340);
+            this.LoadButton.Name = "LoadButton";
+            this.LoadButton.Size = new System.Drawing.Size(83, 37);
+            this.LoadButton.TabIndex = 4;
+            this.LoadButton.Text = "Load Attributes";
+            this.LoadButton.UseVisualStyleBackColor = true;
+            this.LoadButton.Click += new System.EventHandler(this.LoadButton_Click);
+            // 
             // ExtAttEdit
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(466, 389);
+            this.Controls.Add(this.LoadButton);
+            this.Controls.Add(this.GravityUD);
             this.Controls.Add(this.SaveButton);
             this.Controls.Add(this.Gravity);
-            this.Controls.Add(this.numericUpDown1);
             this.Name = "ExtAttEdit";
             this.Text = "External Attribute Editor";
-            this.Load += new System.EventHandler(this.Form1_Load);
-            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.GravityUD)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
         }
 
         #endregion
-
-        private System.Windows.Forms.NumericUpDown numericUpDown1;
         private System.Windows.Forms.Label Gravity;
         private System.Windows.Forms.Button SaveButton;
+        private System.Windows.Forms.NumericUpDown GravityUD;
+        private System.Windows.Forms.Button LoadButton;
     }
 }
 
--- ExternalAttributeEditor/Form1.cs
+++ ExternalAttributeEditor/Form1.cs
@@ -15,22 +15,34 @@ namespace ExternalAttributeEditor
         BinaryReader reader;
         BinaryWriter writer;
         Stream attribFilePath;
+        int gravity;
         public ExtAttEdit()
         {
             InitializeComponent();
             attribFilePath = File.Open("..\\..\\..\\GDAPS2Game\\Content\\AttributeEditor\\attributes.dat", FileMode.OpenOrCreate);
             reader = new BinaryReader(attribFilePath);
             writer = new BinaryWriter(attribFilePath);
-        }
-
-        private void Form1_Load(object sender, EventArgs e)
-        {
-
+            
+            
         }
 
         private void SaveButton_Click(object sender, EventArgs e)
         {
+            //convert values to ints
+            gravity = (int)GravityUD.Value;
 
+            //output to attributes.dat
+            writer.Write(gravity);
+        }
+
+        private void LoadButton_Click(object sender, EventArgs e)
+        {
+            //read in values
+            gravity = reader.ReadInt32();
+
+
+            //update values
+            GravityUD.Value = gravity;
         }
     }
 }
